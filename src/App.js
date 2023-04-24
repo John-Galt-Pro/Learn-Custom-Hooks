694f21868b72e73f78f215fb2bd18cc8cd041f6f
John_Galt <john.galt.prog@gmail.com>
2023-04-24 14:16:29 +0300

    Validation of a controlled form

--- src/App.js
+++ src/App.js
@@ -4,18 +4,37 @@ import './App.css';
 
 const Form = () => {
     const [text, setText] = useState('');
+    const [textArea, setTextArea] = useState('');
+
+
+    const validateInput = (text) => {
+        return text.search(/\d/) >= 0
+    }
+
+    const color = validateInput(text) ? 'text-danger' : null;
+
 
     return (
         <Container>
             <form className="w-50 border mt-5 p-3 m-auto">
                 <div className="mb-3">
-                    <input value={text} type="text" className="form-control" readOnly/>
+                    <input value={`${text} / ${textArea}`} type="text" className="form-control" readOnly/>
                     <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                    <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    <input
+                        onChange={(e) => setText(e.target.value)}
+                        type="email"
+                        value={text}
+                        className={`form-control ${color}`}
+                        id="exampleFormControlInput1"
+                        placeholder="name@example.com"/>
                     </div>
                     <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea
+                        onChange={e => setTextArea(e.target.value)}
+                        className="form-control"
+                        id="exampleFormControlTextarea1"
+                        rows="3"></textarea>
                 </div>
             </form>
         </Container>
@@ -29,3 +48,5 @@ function App() {
 }
 
 export default App;
+
+
